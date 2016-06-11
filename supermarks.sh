57e789a274e4ce8d98aaa94dbc15054fe55117ed
Matt Nichols <matt@mattnichols.net>
2016-06-11 16:05:07 -0700

    Renaming & grooming

---
+++ supermarks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source supermarks.cfg
+
+if [ ! -f supermarks ]; then
+  echo "Building executable from Go source..."
+  go build supermarks.go
+fi
+
+echo "Generating HTML page..."
+./supermarks -bookmarks "$chrome_bookmarks_path" -output $output_file_name -root "$root_folder"
+
+if [ ! -z "$scp_destination" ]; then
+  echo "Uploading..."
+  scp $output_file_name $scp_destination
+else
+  echo "Upload disabled."
+fi
+
+echo "Done!"
